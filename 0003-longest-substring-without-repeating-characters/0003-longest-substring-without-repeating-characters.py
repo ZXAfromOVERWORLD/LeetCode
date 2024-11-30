a1abbfd8ef1daa6053d37cb7a85eeeefc096437f
ZXAfromOVERWORLD <167204215+ZXAfromOVERWORLD@users.noreply.github.com>
2024-11-30 13:42:51 +0530

    Time: 15 ms (80.81%), Space: 17.4 MB (5.05%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = answer = 0
+        chars = [-1]*128
+        print(chars)
+        for r, ch in enumerate(s):
+            if chars[ord(ch)] >= l:
+                l = chars[ord(ch)] +1
+            else:
+                answer = max(answer, r-l+1)
+            chars[ord(ch)] = r
+        return answer
