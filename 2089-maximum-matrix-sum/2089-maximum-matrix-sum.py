670704dab7de10b09d4d4f5501c701eadef3bde2
ZXAfromOVERWORLD <167204215+ZXAfromOVERWORLD@users.noreply.github.com>
2024-12-02 00:47:05 +0530

    Time: 95 ms (36.46%), Space: 26.1 MB (5.83%) - LeetHub

---
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        l=[]
+        count=0
+        flag=0
+        zero=0
+        s=0
+        for i in matrix:
+            l.extend(i)
+        l.sort()
+        for i in l:
+            if i<0:
+                count+=1
+        if 0 in l:
+            for i in l:
+                s+=abs(i)
+        elif count%2==0:
+            for i in l:
+                s+=abs(i)
+        else:
+            for i in range(len(l)):
+                l[i]=abs(l[i])
+            l.sort()
+            l[0]*=-1
+            s=sum(l)
+        return s
+
+        
