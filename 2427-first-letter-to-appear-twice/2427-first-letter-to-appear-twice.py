7168f3c9b3074a0f297d691aa59bddfbf27309cf
ZXAfromOVERWORLD <167204215+ZXAfromOVERWORLD@users.noreply.github.com>
2024-12-06 20:02:50 +0530

    Time: 0 ms (100%), Space: 17.2 MB (8.36%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        t=set()
+        for i in s:
+            if i in t:
+                return i
+            t.add(i)
+        
